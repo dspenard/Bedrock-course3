c2df44b22b441dd12ca35efd4af8db9c6db6abe7
David Spenard <dspenard@users.noreply.github.com>
2023-12-22 16:10:56 -0500

    Update code_generation.py

--- code_generation.py
+++ code_generation.py
@@ -19,11 +19,17 @@ Human: Write {language} code for the following instructions: {message}.
         "stop_sequences":["\n\nHuman:"]
     }
 
+    print(prompt_text)
+    print(body)
+    
     try:
-        bedrock = boto3.client("bedrock-runtime",region_name="us-west-2",config = botocore.config.Config(read_timeout=300, retries = {'max_attempts':3}))
+        #bedrock = boto3.client("bedrock-runtime",region_name="us-east-1",config = botocore.config.Config(read_timeout=10000, retries = {'max_attempts':3}))
+        bedrock = boto3.client("bedrock-runtime")
+        print('called bedrock')
         response = bedrock.invoke_model(body=json.dumps(body),modelId="anthropic.claude-v2")
         response_content = response.get('body').read().decode('utf-8')
         response_data = json.loads(response_content)
+        print(response_data)
         code = response_data["completion"].strip()
         return code
 
@@ -31,9 +37,14 @@ Human: Write {language} code for the following instructions: {message}.
         print(f"Error generating the code: {e}")
         return ""
 
+
 def save_code_to_s3_bucket(code, s3_bucket, s3_key):
 
+    print(s3_bucket)
+    print(s3_key)
+    
     s3 = boto3.client('s3')
+    print("got s3 client")
 
     try:
         s3.put_object(Bucket = s3_bucket, Key = s3_key, Body = code)
@@ -42,8 +53,15 @@ def save_code_to_s3_bucket(code, s3_bucket, s3_key):
     except Exception as e:
         print("Error when saving the code to s3")
 
+
 def lambda_handler(event, context):
-    event = json.loads(event['body'])
+    
+    try:
+        event = json.loads(event['body'])
+    except Exception as e:
+        print("no body element")
+        
+    print(event)
     message = event['message']
     language = event['key']
     print(message, language)
@@ -51,9 +69,10 @@ def lambda_handler(event, context):
     generated_code = generate_code_using_bedrock(message, language)
 
     if generated_code:
+        print('code was generated')
         current_time = datetime.now().strftime('%H%M%S')
         s3_key = f'code-output/{current_time}.py'
-        s3_bucket = 'bedrock-course-bucket'
+        s3_bucket = 'my-bucket'
 
         save_code_to_s3_bucket(generated_code,s3_bucket,s3_key)
 
@@ -63,5 +82,4 @@ def lambda_handler(event, context):
     return {
         'statusCode':200,
         'body':json.dumps('Code generation ')
-
     }
